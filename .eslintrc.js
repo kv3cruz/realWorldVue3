7c56b07e143f53eef616270ecde079bf9071d007
kv3delacruz <kv3delacruz@gmail.com>
2021-07-20 10:58:36 -0600

    init proyect vue3

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,25 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/prettier"],
-  parserOptions: {
-    parser: "babel-eslint",
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
-};
+   root: true,
+   env: {
+      node: true
+   },
+   //plugins: ['vuetify'],
+   extends: [
+      'plugin:vue/recommended',
+      'eslint:recommended',
+      'plugin:prettier/recommended',
+      'prettier/vue'
+   ],
+   rules: {
+      'no-unreachable': 'off',
+      'vue/valid-v-slot': 'off',
+      //'vuetify/no-deprecated-classes': 'error',
+      'vue/no-side-effects-in-computed-properties': 'off',
+      'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+      'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+   },
+   parserOptions: {
+      parser: 'babel-eslint'
+   }
+}
