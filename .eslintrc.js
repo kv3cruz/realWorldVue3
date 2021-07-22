12c0f09dc375a234a8a53a1e25573823b5e6d0e4
kv3delacruz <kv3delacruz@gmail.com>
2021-07-22 10:09:33 -0600

    Hot fix in eslint rules production

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,11 +13,8 @@ module.exports = {
    rules: {
       'no-unreachable': 'off',
       'vue/valid-v-slot': 'off',
-      'no-console': 'off',
-      //'vuetify/no-deprecated-classes': 'error',
       'vue/no-side-effects-in-computed-properties': 'off',
       'vue/component-name-in-template-casing': ['error', 'PascalCase'],
-      'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
       'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
    },
    parserOptions: {
