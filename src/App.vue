5fd050141c5229513dd3d056885334732ca9bfdc
kv3delacruz <kv3delacruz@gmail.com>
2021-07-23 15:03:06 -0600

    Flash messages included

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,34 @@
 <template>
    <div id="nav">
+      <div v-if="GStore.flashMessage" id="flashMessage">
+         {{ GStore.flashMessage }}
+      </div>
       <router-link :to="{ name: 'EventList' }">Event List</router-link> |
       <router-link :to="{ name: 'About' }">About</router-link>
-
       <router-view />
    </div>
 </template>
+<script>
+export default {
+   inject: ['GStore']
+}
+</script>
 
 <style>
+@keyframes yellowfade {
+   from {
+      background: yellow;
+   }
+   to {
+      background: transparent;
+   }
+}
+
+#flashMessage {
+   animation-name: yellowfade;
+   animation-duration: 3s;
+   margin-bottom: 50px;
+}
 #app {
    font-family: Avenir, Helvetica, Arial, sans-serif;
    -webkit-font-smoothing: antialiased;
